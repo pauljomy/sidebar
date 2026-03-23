b819fb6ef9d3ca5d9cfa887835f44d277181421a
Paul Jomy <pauljomy@users.noreply.github.com>
2026-03-23 06:56:19 +0530

    working in side bar items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,28 +2,28 @@ import { LayoutGrid, Film, Tv, Bookmark, X, Menu } from "lucide-react";
 import { useState } from "react";
 
 const Sidebar = ({ isOpen, setIsOpen }) => {
-  const [activeState, setActiveState] = useState();
+  const [activeDropDown, setActiveDropDown] = useState();
 
-  // const navItems = [
-  //   { title: "Dashboard", icon: LayoutGrid, hasDropdown: false },
-  //   {
-  //     title: "Movies",
-  //     icon: Film,
-  //     hasDropdown: true,
-  //     dropdownItems: ["Trending", "Top Rated", "Upcoming", "Genres"],
-  //   },
-  //   {
-  //     title: "TV Shows",
-  //     icon: Tv,
-  //     hasDropdown: true,
-  //     dropdownItems: ["Popular", "Airing Today", "On The Air", "Top Rated"],
-  //   },
-  //   { title: "Watchlist", icon: Bookmark, hasDropdown: false },
-  // ];
+  const navItems = [
+    { title: "Dashboard", icon: LayoutGrid, hasDropdown: false },
+    {
+      title: "Movies",
+      icon: Film,
+      hasDropdown: true,
+      dropdownItems: ["Trending", "Top Rated", "Upcoming", "Genres"],
+    },
+    {
+      title: "TV Shows",
+      icon: Tv,
+      hasDropdown: true,
+      dropdownItems: ["Popular", "Airing Today", "On The Air", "Top Rated"],
+    },
+    { title: "Watchlist", icon: Bookmark, hasDropdown: false },
+  ];
 
   return (
     <div
-      className={`bg-white rounded-sm drop-shadow-sm h-full ${isOpen ? "w-64" : "w-14"} transition-all ease-in-out`}
+      className={`bg-white rounded-sm drop-shadow-sm h-full ${isOpen ? "w-64" : "w-14"} transition-all duration-300 ease-in-out`}
     >
       <button
         onClick={() => setIsOpen(!isOpen)}
@@ -31,6 +31,20 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
       >
         {isOpen ? <X size={24} /> : <Menu size={24} />}
       </button>
+      <div className={`mt-15`}>
+        {navItems.map((item) => {
+          return (
+            <div key={item.title}>
+              <div className={`flex items-center gap-2 px-3 mb-3`}>
+                <item.icon size={20} />
+                <span className={`transition-all duration-300`}>
+                  {isOpen ? item.title : ""}
+                </span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
